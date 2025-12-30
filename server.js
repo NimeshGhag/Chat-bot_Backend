df48b35e0875c47c7048eba0a808b6e32cdaa78b
NimeshGhag <ghagnimesh490@gmail.com>
2025-12-30 12:15:39 +0530

    feat: add and initialize Socket.io with basic connection events

--- server.js
+++ server.js
@@ -3,6 +3,21 @@ require("dotenv").config();
 const app = require("./src/app");
 const port = process.env.PORT;
 
-app.listen(port, () => {
+const { createServer } = require("http");
+const { Server } = require("socket.io");
+
+const httpServer = createServer(app);
+const io = new Server(httpServer, {
+  /* options */
+});
+
+io.on("connection", (socket) => {
+  console.log("A user is connected");
+  socket.on("disconnect", () => {
+    console.log("A user Disconnected");
+  });
+});
+
+httpServer.listen(port, () => {
   console.log("Server is running on port");
 });
